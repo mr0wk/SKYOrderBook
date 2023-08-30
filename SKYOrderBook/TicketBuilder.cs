7d61983a15561f7690903d309147fbcbd3591020
mrowk <mrowkamaksymilian@gmail.com>
2023-08-30 16:26:01 +0200

    Add major refectorings to TicketBuilder class.

--- SKYOrderBook/TicketBuilder.cs
+++ SKYOrderBook/TicketBuilder.cs
@@ -22,50 +22,11 @@ namespace SKYOrderBook
                     continue;
                 }
 
-                var processedTicksToOperateOn = tick.Side == 1 ? _processedBidTicks : _processedAskTicks;
-                var processedSortedTicksToOperateOn = tick.Side == 1 ? _processedBidTicksSortedByPrice : _processedAskTicksSortedByPrice;
-
-                if (!processedTicksToOperateOn.ContainsKey(tick.OrderId))
-                {
-                    if (tick.Action == RecordAction.A)
-                    {
-                        processedTicksToOperateOn.Add(tick.OrderId, tick);
-
-                        if (processedSortedTicksToOperateOn.ContainsKey(tick.Price))
-                        {
-                            processedSortedTicksToOperateOn[tick.Price].Add(tick);
-                        }
-                        else
-                        {
-                            processedSortedTicksToOperateOn.Add(tick.Price, new List<CsvRecord>() { tick });
-                        }
-                    }
-                }
-                else
-                {
-                    if (tick.Action == RecordAction.M)
-                    {
-                        processedTicksToOperateOn.Remove(tick.OrderId);
-                        processedSortedTicksToOperateOn[tick.Price].RemoveAll(existingTick => existingTick.OrderId == tick.OrderId);
-
-                        processedTicksToOperateOn.Add(tick.OrderId, tick);
-                        processedSortedTicksToOperateOn[tick.Price].Add(tick);
-                    }
-                    else if (tick.Action == RecordAction.D)
-                    {
-                        processedTicksToOperateOn.Remove(tick.OrderId);
-                        processedSortedTicksToOperateOn[tick.Price].RemoveAll(existingTick => existingTick.OrderId == tick.OrderId);
-                    }
-                }
+                ProcessTick(tick);
 
                 UpdateTicketComponents((byte)tick.Side);
 
-                tick.A0 = _a0;
-                tick.AQ0 = _aq0;
-                tick.AN0 = _an0;
-                tick.B0 = _b0;
-                tick.BQ0 = _bq0;
-                tick.BN0 = _bn0;
+                UpdateTicketProperties(tick);
 
                 _ticket.Add(tick);
             }
@@ -83,21 +44,92 @@ namespace SKYOrderBook
             _b0 = _bq0 = _bn0 = _a0 = _aq0 = _an0 = null;
         }
 
+        private void ProcessTick(CsvRecord tick)
+        {
+            var processedTicks = tick.Side == 1 ? _processedBidTicks : _processedAskTicks;
+            var processedSortedTicks = tick.Side == 1 ? _processedBidTicksSortedByPrice : _processedAskTicksSortedByPrice;
+
+            if (!processedTicks.ContainsKey(tick.OrderId))
+            {
+                if (tick.Action == RecordAction.A)
+                {
+                    AddTickToProcessed(processedTicks, processedSortedTicks, tick);
+                }
+            }
+            else
+            {
+                if (tick.Action == RecordAction.M)
+                {
+                    UpdateTickInProcessed(processedTicks, processedSortedTicks, tick);
+                }
+                else if (tick.Action == RecordAction.D)
+                {
+                    RemoveTickFromProcessed(processedTicks, processedSortedTicks, tick);
+                }
+            }
+        }
+
+        private void AddTickToProcessed(Dictionary<ulong, CsvRecord> processedTicks, Dictionary<ushort, List<CsvRecord>> processedSortedTicks, CsvRecord tick)
+        {
+            processedTicks.Add(tick.OrderId, tick);
+
+            if (processedSortedTicks.ContainsKey(tick.Price))
+            {
+                processedSortedTicks[tick.Price].Add(tick);
+            }
+            else
+            {
+                processedSortedTicks.Add(tick.Price, new List<CsvRecord> { tick });
+            }
+        }
+
+        private void UpdateTickInProcessed(Dictionary<ulong, CsvRecord> processedTicks, Dictionary<ushort, List<CsvRecord>> processedSortedTicks, CsvRecord tick)
+        {
+            RemoveTickFromProcessed(processedTicks, processedSortedTicks, tick);
+
+            AddTickToProcessed(processedTicks, processedSortedTicks, tick);
+        }
+
+        private void RemoveTickFromProcessed(Dictionary<ulong, CsvRecord> processedTicks, Dictionary<ushort, List<CsvRecord>> processedSortedTicks, CsvRecord tick)
+        {
+            processedTicks.Remove(tick.OrderId);
+            processedSortedTicks[tick.Price].RemoveAll(existingTick => existingTick.OrderId == tick.OrderId);
+        }
 
         private void UpdateTicketComponents(byte side)
         {
             if (side == 1)
             {
-                _b0 = _processedBidTicks.Values.Max(t => t.Price);
-                _bq0 = (ushort)_processedBidTicksSortedByPrice[(ushort)_b0].Sum(t => t.Quantity);
-                _bn0 = (ushort)_processedBidTicksSortedByPrice[(ushort)_b0].Count();
+                UpdateBidComponents();
             }
             else if (side == 2)
             {
-                _a0 = _processedAskTicks.Values.Min(t => t.Price);
-                _aq0 = (ushort)_processedAskTicksSortedByPrice[(ushort)_a0].Sum(t => t.Quantity);
-                _an0 = (ushort)_processedAskTicksSortedByPrice[(ushort)_a0].Count();
+                UpdateAskComponents();
             }
+        }
+
+        private void UpdateBidComponents()
+        {
+            _b0 = _processedBidTicks.Values.Max(t => t.Price);
+            _bq0 = (ushort)_processedBidTicksSortedByPrice[(ushort)_b0].Sum(t => t.Quantity);
+            _bn0 = (ushort)_processedBidTicksSortedByPrice[(ushort)_b0].Count();
+        }
+
+        private void UpdateAskComponents()
+        {
+            _a0 = _processedAskTicks.Values.Min(t => t.Price);
+            _aq0 = (ushort)_processedAskTicksSortedByPrice[(ushort)_a0].Sum(t => t.Quantity);
+            _an0 = (ushort)_processedAskTicksSortedByPrice[(ushort)_a0].Count();
+        }
+
+        private void UpdateTicketProperties(CsvRecord tick)
+        {
+            tick.A0 = _a0;
+            tick.AQ0 = _aq0;
+            tick.AN0 = _an0;
+            tick.B0 = _b0;
+            tick.BQ0 = _bq0;
+            tick.BN0 = _bn0;
         }
     }
 }
